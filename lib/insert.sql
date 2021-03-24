34f7405fd9b316595b3c75bda5cf049c37095387
maryamzain123 <smaryam.zain@gmail.com>
2021-03-24 07:44:19 -0500

    commit tree db11bdd45ae96fc45a0e70c399a666c4866ba3ff

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT into bears ( name, age , sex , color , temperament ,
+    alive ) VALUES( "Mr.Chocolate", 20, "M", "brown", "angry", 1);
+
+    INSERT into bears ( name, age , sex , color , temperament ,
+    alive ) VALUES( "Rowdy", 10, "M", "brown", "angry", 1);
+
+    INSERT into bears ( name, age , sex , color , temperament ,
+    alive ) VALUES( "Tabitha", 6, "female", "black", "sad", 1);
+
+    INSERT into bears ( name, age , sex , color , temperament ,
+    alive ) VALUES("Sergeant Brown", 4, "M", "white", "sad", 1);
+
+    INSERT into bears ( name, age , sex , color , temperament ,
+    alive ) VALUES( "Melissa", 13, "F", "brown", "happy", 1);
+
+    INSERT into bears ( name, age , sex , color , temperament ,
+    alive ) VALUES("Grinch", 2, "M", "brown", "angry", 1);
+
+    INSERT into bears ( name, age , sex , color , temperament ,
+    alive ) VALUES( "Wendy", 6, "F", "brown", "angry", 1);
+
+    INSERT into bears ( name, age , sex , color , temperament ,
+    alive ) VALUES( NULL, 10, "M", "brown", "angry", 1);
+    
+
--- lib/seed.sql
+++ lib/seed.sql
@@ -7,3 +7,12 @@ INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
 (6,"Grinch", 2, "M", "Black", "Grinchy", 1),
 (7,"Wendy", 6, "F", "Blue", "naive", 1),
 (8,null, 20, "M", "black", "aggressive", 0);
+
+-- SELECT bears.name FROM bears ORDER BY bears.name ASC;
+-- SELECT bears.name, bears.age FROM bears WHERE sex is 'F';
+-- SELECT bears.name, bears.age FROM bears WHERE alive is 1 ORDER BY bears.age DESC;
+-- SELECT bears.name, bears.age FROM bears ORDER BY age DESC ;
+-- SELECT bears.name, bears.age FROM bears ORDER BY age DESC ;
+
+
+
